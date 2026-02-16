ac00bbc33123a38b0916530dcf05bf89f3fec72d
Mr. Phoenix <69739537+zeoenix@users.noreply.github.com>
2026-02-16 16:49:37 +0530

    updated api

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5rlaBn32luodgUYPM7IPxdvVzndnkNJM",
-  authDomain: "image-compressor-fd45b.firebaseapp.com",
-  projectId: "image-compressor-fd45b",
-  storageBucket: "image-compressor-fd45b.firebasestorage.app",
-  messagingSenderId: "758559330120",
-  appId: "1:758559330120:web:207c6463ca8479c3092d7d",
-  measurementId: "G-FLJHP5DXN4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
